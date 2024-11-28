df0261a3f7fa9ec3f9759d11b146ea95dd5aa7aa
mintuchel <max3627@naver.com>
2024-11-28 15:46:37 +0900

    fix: joinservice

--- src/main/java/v1/domain/service/JoinService.java
+++ src/main/java/v1/domain/service/JoinService.java
@@ -1,7 +1,7 @@
 package v1.domain.service;
 
 import v1.domain.dto.JoinDTO;
-import v1.domain.entity.User;
+import v1.domain.entity.UserEntity;
 import v1.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,27 +12,23 @@ import org.springframework.stereotype.Service;
 public class JoinService {
 
     private final UserRepository userRepository;
-    // password 암호화를 위한
+    // password 암호화를 위한 Bean 객체 주입받기
+    // 이 Bean 객체는 SecurityConfig 에서 생성한 빈임
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public void joinProcess(JoinDTO joinDTO){
         String username = joinDTO.username();
         String password = joinDTO.password();
 
-        Boolean isExist = userRepository.existsByUsername(username);
+        Boolean doesExist = userRepository.existsByUsername(username);
 
-        if(isExist){
-            return;
-        }
+        if(doesExist) return;
 
-        User newUser = User.builder()
-                .username(username)
-                // password 는 무조건 암호화를 진행해서 넣어야함!!
-                // 바로 .password(password) 하면 안됨!!
-                .password(bCryptPasswordEncoder.encode(password))
-                .role("ROLE_ADMIN")
-                .build();
-
+        UserEntity newUser = new UserEntity();
+        newUser.setUsername(username);
+        // password 는 무조건 암호화해서 넣어줘야함!!
+        newUser.setPassword(bCryptPasswordEncoder.encode(password));
+        newUser.setRole("ROLE_ADMIN");
         userRepository.save(newUser);
     }
 }
