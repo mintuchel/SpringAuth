ef33577e3bbfd97d58dd50f286d0de40c4318ad8
mintuchel <max3627@naver.com>
2025-03-18 15:57:58 +0900

    fix: uri

--- src/main/java/v1/domain/controller/AdminController.java
+++ src/main/java/v1/domain/controller/AdminController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
 public class AdminController {
     @GetMapping("/admin")
     public String adminP(){
--- src/main/java/v1/domain/controller/MainController.java
+++ src/main/java/v1/domain/controller/MainController.java
@@ -5,9 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
 public class MainController {
-    @GetMapping("")
+    @GetMapping("/")
     public String mainP(){
         return "Main Controller";
     }
