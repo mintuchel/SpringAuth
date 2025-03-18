338606b881179a45c445e030fcd954006251789e
mintuchel <max3627@naver.com>
2025-03-18 14:36:07 +0900

    refactor: SecurityConfig

--- src/main/java/v1/global/security/config/SecurityConfig.java
+++ src/main/java/v1/global/security/config/SecurityConfig.java
@@ -20,14 +20,38 @@ import v1.global.security.filters.JwtFilter;
 import java.util.Collections;
 
 /**
- * 이 SecurityConfig 파일 안에 SecurityFilter"Chain" 을 반환하는 @Bean 을 정의해둠으로써
- * WAS 톰캣 필터 체인이 아닌! Spring Security 필터체인을 커스텀해서 등록할 수 있음
- * 이 Spring Security 필터체인은 톰캣 필터체인과 달리 스프링 컨테이너 내부에서 동작함!!
-**/
+ * @Configuration -> 해당 클래스가 설정 클래스임을 Spring 에게 알림
+ * 내부의 @Bean 메서드들이 Spring 컨테이너에 등록됨
+ *
+ * @EnableWebSecurity -> Spring Security 활성화
+ * 기본적인 Spring Security 필터 체인이 등록되고 커스텀할 수 있게 해줌
+ */
 
 @Configuration
-@EnableWebSecurity // Spring Security 를 활성화하겠다. 필터 체인을 생성하고 웹 보안을 활성화하겠다
+@EnableWebSecurity
 public class SecurityConfig {
+
+    // AuthenticationManager에 필요한 AuthenticationConfiguration 정의
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    private final JwtProvider jwtProvider;
+
+    // LoginFilterChain(UsernamePasswordAuthenticationFilter)에 필요한 AuthenticationManager 를 @Bean 으로 등록
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration, JwtProvider jwtProvider){
+        this.authenticationConfiguration = authenticationConfiguration;
+        this.jwtProvider = jwtProvider;
+    }
+
+    // AuthenticationManager 에 필요한 PasswordEncoder @Bean 으로 등록
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     // 내가 Security Logic 에 등록하고 싶은 새로운 하나의 customFilterChain 에 대한 정의
     @Bean
@@ -56,11 +80,9 @@ public class SecurityConfig {
                     }
                 })));
 
-        // Cross-Site Request Forgery 공격 방어 기능을 비활성화
-        // REST API 나 JWT 토큰 기반 인증에서는 보통 서버가 세션을 사용하지 않기 때문에 CSRF 가 불필요할때가 많음
+
         http.csrf((auth) -> auth.disable());
-        // Form 로그인 방식 disable
-        // 이 설정은 클라이언트에서 별도의 로그인 페이지를 사용하거나 다른 인증 방식을 사용할때 필요함
+
         http.formLogin((auth) -> auth.disable());
         // HTTP 기본 인증 방식 비활성화
         // 브라우저에서 기본적으로 제공하는 팝업 창을 통해 사용자 인증을 처리하는 방식인데,
@@ -74,50 +96,17 @@ public class SecurityConfig {
                         .anyRequest().authenticated());
 
         /**
-         * Request Header 를 통해 토큰 유효성을 검증해주는 JwtFilter
-         *  로그인 인증과 필요시 Jwt Token 발급을 해주는 CustomLoginFilter
-         *  이 두 개의 커스텀 필터를 SecurityFilterChain 에 추가하는 작업이 아래 두 줄임
-         *  Spring Security 의 Default 필터 체인에서 UsernamePasswordAuthenticationFilter 위치에 추가
-         *  이 필터가 JWT 기반 인증을 처리하고 클라이언트에서 제공한 JWT 토큰을 검증하거나 발급하는 역할을 함
+         * JwtFilter 와 LoginFilter 를 SecurityFilterChain 에 추가
          */
-
         http.addFilterBefore(new JwtFilter(jwtProvider), LoginFilter.class);
         http.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtProvider), UsernamePasswordAuthenticationFilter.class);
 
         // 세션 설정
-        // jwt는 세션을 항상 stateless로 관리
+        // Jwt 는 당연히 stateless로 관리
         // 이 부분이 가장 중요함!
         http.sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-        // http.build 를 통해 SecurityFilterChain 객체를 반환
-        // 이 객체는 HTTP 요청과 관련된 모든 보안 설정을 담고 있고
-        // Spring Security 에서 요청을 처리할때 위에서 해준 설정을 기준으로 동작함
         return http.build();
-    }
-
-    // CustomLoginFilter 에 필요한 AuthenticationManager 를 생성하기 위한 AuthenticationConfiguration 주입받기
-    private final AuthenticationConfiguration authenticationConfiguration;
-
-    // CustomLoginFilter 에 필요한 JWTUtil 주입받기
-    private final JwtProvider jwtProvider;
-
-    // 위에 생성한 CustomFilterChain 인 loginFilterChain 생성자의 매개변수로 전달해야하는 AuthenticationManager 를 빈으로 등록
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
-        return configuration.getAuthenticationManager();
-    }
-
-    // 위에꺼 생성자로 주입
-    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration, JwtProvider jwtProvider){
-        this.authenticationConfiguration = authenticationConfiguration;
-        this.jwtProvider = jwtProvider;
-    }
-
-    // Spring Security 를 통해서 회원 정보를 저장할때는 암호화해서 저장함
-    // 이 password 암호화 Bean 은 추후 Service 단에서 해당 Bean 을 주입받아서 쓰게됨
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder(){
-        return new BCryptPasswordEncoder();
     }
 }
