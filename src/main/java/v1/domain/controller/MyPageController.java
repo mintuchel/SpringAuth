d1a3423055ef74c4a369e6c51b8b11e6d3d1298b
mintuchel <max3627@naver.com>
2025-04-24 19:48:22 +0900

    add: authentication principal to parse user details

--- src/main/java/v1/domain/controller/MyPageController.java
+++ src/main/java/v1/domain/controller/MyPageController.java
@@ -1,12 +1,19 @@
 package v1.domain.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import v1.global.security.model.JwtUserDetails;
 
 @RestController
 public class MyPageController {
     @GetMapping("/mypage")
-    public String handleMyPage(){
+    public String handleMyPage(@AuthenticationPrincipal JwtUserDetails userDetails){
+        System.out.println("current user");
+        System.out.println("id :" + userDetails.getId());
+        System.out.println("username : " + userDetails.getUsername());
+        System.out.println("password : " + userDetails.getPassword());
+        System.out.println("authorities : " + userDetails.getAuthorities());
         return "MyPageController";
     }
 }
